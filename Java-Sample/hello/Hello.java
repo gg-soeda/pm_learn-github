91ee678d2514c91975411a1fa64858340353efbc
Ryoji Soeda <soeda-r@groove-gear.co.jp>
2021-12-01 10:20:31 +0900

    Revert "Revert "Merge branch 'master' into pr/2""
    This reverts commit 771dac8c1077c01db4696a5873d3c79219a8702a.

---
+++ Java-Sample/hello/Hello.java
@@ -0,0 +1,11 @@
+package hello;
+
+// 作成したクラスをインポート
+import hello.HelloTakeuchi;
+
+class Hello {
+  public static void main(String[] args) {
+    System.out.println(HelloTakeuchi.greet());
+
+  }
+}
